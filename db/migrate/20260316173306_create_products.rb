62ffab84f2695c5ed7655e542b447918a016493d
Peyton Blanchard <143459422+peytxn@users.noreply.github.com>
2026-03-20 17:27:53 +0000

    Followed all steps

---
+++ db/migrate/20260316173306_create_products.rb
@@ -0,0 +1,10 @@
+# db/migrate/<timestamp>_create_products.rb
+class CreateProducts < ActiveRecord::Migration[8.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
